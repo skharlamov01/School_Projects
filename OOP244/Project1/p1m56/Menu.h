7ca90373fa33318c6d1f4bb381596333581d3cd0
Sasha Kharlamov <skharlamov@myseneca.ca>
2022-09-14 16:33:17 -0400

    removed all unnecessary milestones except final product, removed student id from all files in final milestone

--- OOP244/Project1/p1m56/Menu.h
+++ OOP244/Project1/p1m56/Menu.h
@@ -3,7 +3,6 @@ Module : Menu
 Filename : Menu.h
 Version 1.0
 Author : Sasha Kharlamov
-Author ID : 150707214
 Date : 2022/04/10
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
